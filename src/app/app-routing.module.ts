a712c836a8490690c51660fedb32b3a9d37256a3
MajinM92160 <mkacem6@outlook.com>
2022-11-29 18:16:30 +0100

    création et configuration de la page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
 import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: 'pokemon', component: ListPokemonComponent },
   { path: 'pokemon/:id', component: DetailPokemonComponent},
-  { path: '', redirectTo: 'pokemon', pathMatch: 'full' }
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
