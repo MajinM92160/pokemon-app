eb844eb0b92b8f8c4472927e19444b068cf92a23
MajinM92160 <mkacem6@outlook.com>
2023-01-07 10:04:27 +0100

    return in my project

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
 import {POKEMONS} from "./mock-pokemon-list";
 import {Pokemon} from "./pokemon";
+import {HttpClient} from "@angular/common/http";
+import {catchError, Observable} from "rxjs";
 
 @Injectable()
 export class PokemonService {
 
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
-  }
+    constructor(private http : HttpClient) {}
+
+  /*getPokemonList(): Observable<Pokemon[]>{
+   return this.http.get<Pokemon[]>('api/pokemons').pipe(
+       tap((response)=> console.table(response)),
+       catchError((error) => {
+           console.log(error);
+           return of([]);
+       })
+   )
+  }*/
 
   getPokemonById(pokemonId: number): Pokemon | undefined {
     return POKEMONS.find(pokemon => pokemon.id == pokemonId);
